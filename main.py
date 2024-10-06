6625d8b7c4aea59ac9647c6423651944a6150ab1
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2024-10-06 16:50:53 +0800

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Router, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from settings import TOKEN
+
+bot = Bot(token=TOKEN)
+router = Router()
+
+
+@router.message(Command(commands=['start']))
+async def command_start_handler(message: Message):
+    await message.answer('Hi!!!')
+
+
+async def main():
+    dp = Dispatcher()
+    dp.include_routers(
+        router,
+    )
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
