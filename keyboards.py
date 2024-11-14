f55226658685a608e04e1cc650e30062a9c1fec1
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2024-11-14 17:25:37 +0800

    + сканер штрих-кода ред.

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,1 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-menu_buttons = [
-    [KeyboardButton(text='Сканер штрих-кодов')],
-    [KeyboardButton(text='Получить список анализов для плановой госпитализации')],
-]
-menu = ReplyKeyboardMarkup(keyboard=menu_buttons, resize_keyboard=True)
--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import asyncio
 import logging
 
 from aiogram import Bot, Router, Dispatcher, F
-from aiogram.enums import ContentType
 from aiogram.filters import Command
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import Message, KeyboardButton, WebAppInfo
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from handlers import working_with_database
-from settings import TOKEN
+from settings import TOKEN, WEB_APP_URL
 
 bot = Bot(token=TOKEN)
 router = Router()
@@ -16,15 +15,9 @@ router = Router()
 
 @router.message(Command(commands=['start']))
 async def command_start_handler(message: Message):
-    web_app_url = "https://drfess.github.io/warehouseIB/index.html"
-    builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(text="Запустить сканер", web_app=WebAppInfo(url=web_app_url)))
-    await message.answer("Нажмите на кнопку ниже, чтобы открыть сканер:", reply_markup=builder.as_markup())
-
-
-@router.message(F.content_type == ContentType.WEB_APP_DATA)
-async def web_app_handler(message: Message):
-    await message.answer(message.web_app_data)
+    builder = ReplyKeyboardBuilder()
+    builder.row(KeyboardButton(text='Сканер штрих-кода', web_app=WebAppInfo(url=WEB_APP_URL), resize_keyboard=True))
+    await message.answer('Добро пожаловать!', reply_markup=builder.as_markup())
 
 
 async def main():
